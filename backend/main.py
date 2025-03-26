90577267038e0670e9963c29dee9a19fc0062ef0
Luc Vuong <luctranvuong@gmail.com>
2025-03-26 16:14:00 +0700

    add CORS

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from apis import ai, users, meetings
 from db.db_manager import sessionmanager
 from db.models import *
@@ -13,6 +14,16 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan, title="SummarAIze server")
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(ai.router, prefix="/ai", tags=["ai"])
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(meetings.router, prefix="/meetings", tags=["meetings"])
